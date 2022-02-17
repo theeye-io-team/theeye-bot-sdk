e886c2dedc8985721e87539f903654d6e8f7d715
Facundo <facugon@theeye.io>
2022-02-17 15:47:03 -0300

    fixes

--- core/bot/job/index.js
+++ core/bot/job/index.js
@@ -36,31 +36,31 @@ process.on('uncaughtException', err => {
 })
 
 if (process.env.REDIRECT_CONSOLE_OUTPUT) {
-	console = (function(){
-		const logDump = console.log
-		const errorDump = console.error
-		const writeToFile = function (logLevel) {
-			return function (msg) {
-				logDump(msg)
-				try {
-					let logName = `${unitF}:\\facturas\\logs\\${path.parse(filenameArg).name}.log`
-					fs.appendFile(logName, `${logLevel}:${JSON.stringify(msg)}\n`, (e) => {
-						if (e) {
-							logDump('error: ' + e)
-							process.exit(1)
-						}
-					})
-				} catch (e) {
-					errorDump('cannot write to file', e)
-					process.exit(1)
-				}
-			}
-		}
-		console = {}
-		console.log = writeToFile('log')
-		console.error = writeToFile('error')
-		return console
-	})()
+  console = (function(){
+    const logDump = console.log
+    const errorDump = console.error
+    const writeToFile = function (logLevel) {
+      return function (msg) {
+        logDump(msg)
+        try {
+          let logName = `${unitF}:\\facturas\\logs\\${path.parse(filenameArg).name}.log`
+          fs.appendFile(logName, `${logLevel}:${JSON.stringify(msg)}\n`, (e) => {
+            if (e) {
+              logDump('error: ' + e)
+              process.exit(1)
+            }
+          })
+        } catch (e) {
+          errorDump('cannot write to file', e)
+          process.exit(1)
+        }
+      }
+    }
+    console = {}
+    console.log = writeToFile('log')
+    console.error = writeToFile('error')
+    return console
+  })()
 }
 
 class BotJob {
@@ -83,12 +83,9 @@ class BotJob {
 
   async run () {
     try {
-      const parameters = []
-      for (let index = 2; index < process.argv.length; index++) {
-        paramters.push( process.argv[index] )
-      }
+      const parameters = process.argv.slice(2)
 
-      const output = this.run( parameters )
+      const output = await this.main( parameters )
       return successOutput(output)
     } catch (e) {
       return failureOutput(e)
